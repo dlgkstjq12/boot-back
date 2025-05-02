673644136784d173b935b67515581a61e730fbb0
USER <USER@DESKTOP-U640SJO>
2025-05-02 12:31:31 +0900

    [이한섭]commit

---
+++ src/main/java/com/maple/mapleBack/BootMapleBackApplication.java
@@ -0,0 +1,13 @@
+package com.maple.mapleBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMapleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMapleBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maple/mapleBack/BootMapleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.mapleBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMapleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
