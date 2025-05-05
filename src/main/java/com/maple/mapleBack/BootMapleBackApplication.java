86ff7d970472191645cb623bf935f3dd0d301749
admin <admin@DESKTOP-NI69UOR>
2025-05-05 18:15:35 +0900

    [이한섭]환경변수쪽 수정, LOG파일경로 GIT 감지 제외

--- src/main/java/com/maple/mapleBack/BootMapleBackApplication.java
+++ src/main/java/com/maple/mapleBack/BootMapleBackApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootMapleBackApplication {
 
-	public static void main(String[] args) {
-        System.out.println("### Active Profile: " + System.getProperty("spring.profiles.active"));
-		SpringApplication.run(BootMapleBackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootMapleBackApplication.class, args);
+    }
 
 }
